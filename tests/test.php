4c80452969c28217066b233a3962eb19e80a8216
Craig Russell <craig.anthony.russell@gmail.com>
2015-03-14 10:15:09 -0500

    Improved test

--- tests/test.php
+++ tests/test.php
@@ -26,7 +26,7 @@ $test[] = 1;
 $test[] = 2.3;
 $test[] = true;
 $test[] = null;
-$test[] = [[1, 2, 3, 4], 2, 3, 4];
+$test[] = [[1, 2, 3, 4], 2, 3, 4, new B()];
 $test[] = new A();
 $test[] = new B();
 
