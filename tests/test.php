3017705fe3485fea6e8b4729dfa3dc9624a60434
Craig Russell <craig.anthony.russell@gmail.com>
2015-03-14 09:58:53 -0500

    Added some basic tests

---
+++ tests/test.php
@@ -0,0 +1,37 @@
+<?php
+
+include '../d-bug.php';
+
+class A {
+	
+}
+
+class B extends A {
+	public $a = 'test';
+	public $b = [[1, 2, 3, 4], 2, 3, 4];
+	
+	const C = 42;
+	
+	public static function test($a, $b = true) {
+		return true;
+	}
+}
+
+$test = [];
+$test[] = fopen(__FILE__, 'r');
+$test[] = 'test';
+$test[] = 1;
+$test[] = 2.3;
+$test[] = true;
+$test[] = null;
+$test[] = [[1, 2, 3, 4], 2, 3, 4];
+$test[] = new A();
+$test[] = new B();
+
+foreach($test as $k => $t) {
+	echo "Test #", $k, "\n\n";
+	D::bugType($t, false);
+	echo "----------------------------------------\n\n";
+}
+
+fclose($test[0]);
